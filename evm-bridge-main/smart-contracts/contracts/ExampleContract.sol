9a246bfcdbb15fb2df5abe18445c81d9b814c18c
wzjiujiu <62519540+wzjiujiu@users.noreply.github.com>
2025-12-14 18:17:41 +0100

    smart contract update
    update smart contract with policies management

--- evm-bridge-main/smart-contracts/contracts/ExampleContract.sol
+++ evm-bridge-main/smart-contracts/contracts/ExampleContract.sol
@@ -15,6 +15,14 @@ contract ExampleContract is BaseContract {
         uint256 timestamp; // Momento della registrazione
         string title;      // Nuovo campo titolo
     }
+	
+	struct Policy {
+        string id;         // ID univoco dell'asset
+        address owner;     // Indirizzo che ha registrato l'asset
+        uint256 timestamp; // Momento della registrazione
+        string title;      // Nuovo campo titolo
+    }
+	
 
     function initialize(
         address roleManagerAddress,
@@ -24,9 +32,12 @@ contract ExampleContract is BaseContract {
     }
 
     mapping(string => Asset) private assets;
+	mapping(string => Policy) private policies;
 
     event AssetRegistered(address indexed owner, string assetId, uint256 timestamp, string title);
     event AssetModified(string assetId, string newTitle, uint256 timestamp);
+	event PolicyRegistered(address indexed owner, string policyId, uint256 timestamp, string title);
+    event PolicyModified(string policyId, string newTitle, uint256 timestamp);
 
     /// @notice Registra un nuovo asset
     function registerAsset(string memory assetId, string memory assetTitle) external {
@@ -41,6 +52,21 @@ contract ExampleContract is BaseContract {
         });
 
         emit AssetRegistered(msg.sender, assetId, block.timestamp, assetTitle);
+    }
+
+    /// @notice Registra un nuovo policy
+       function registerPolicy(string memory policyId, string memory policyTitle) external {
+        require(bytes(policyId).length > 0, "ID non valido");
+        require(policies[policyId].owner == address(0), "Asset gia' registrato");
+
+        policies[policyId] = Asset({
+            id: policyId,
+            owner: msg.sender,
+            timestamp: block.timestamp,
+            title: policyTitle
+        });
+
+        emit PolicyRegistered(msg.sender, policyId, block.timestamp, policyTitle);
     }
 
     /// @notice Modifica titolo di un asset esistente
@@ -60,6 +86,23 @@ contract ExampleContract is BaseContract {
         emit AssetModified(assetId, newTitle, block.timestamp);
     }
 
+    /// @notice Modifica titolo di un policy esistente
+    function modifyAsset(string memory policyId, string memory newTitle) external {
+        require(bytes(policyId).length > 0, "ID non valido");
+        require(assets[policyId].owner != address(0), "Asset non trovato");
+
+        // Permesso: solo owner o admin
+        require(
+            msg.sender == assets[policyId].owner,
+            "Non autorizzato"
+        );
+
+        assets[policyId].title = newTitle;
+        assets[policyId].timestamp = block.timestamp;
+
+        emit AssetModified(policyId, newTitle, block.timestamp);
+    }
+
     /// @notice Restituisce i dettagli di un asset
     function getAsset(string memory assetId)
         external
@@ -70,6 +113,18 @@ contract ExampleContract is BaseContract {
         require(a.owner != address(0), "Asset non trovato");
         return (a.id, a.owner, a.timestamp, a.title);
     }
+
+    /// @notice Restituisce i dettagli di un policy
+    function getAsset(string memory policyId)
+        external
+        view
+        returns (string memory id, address owner, uint256 timestamp, string memory title)
+    {
+        Asset memory a = policies[policyId];
+        require(a.owner != address(0), "Asset non trovato");
+        return (a.id, a.owner, a.timestamp, a.title);
+    }
+
 
     /// @notice Controlla se un asset è registrato
     function isRegistered(string memory assetId) external view returns (bool) {
