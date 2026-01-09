ccb22e9d0a19cadb045aae2bc5e1e7b4c5ec76c8
wzjiujiu <62519540+wzjiujiu@users.noreply.github.com>
2026-01-09 19:17:40 +0100

    add gasestimation test
    add gasestimation test

--- NousScan/src/components/Navbar.jsx
+++ NousScan/src/components/Navbar.jsx
@@ -1,7 +1,12 @@
+import { Link } from "react-router-dom";
+
 export default function Navbar() {
   return (
     <nav className="navbar">
-      <h1 className="navbar-title">NousScan Explorer</h1>
+      {/* Link al path "/" per tornare alla home */}
+      <Link to="/" className="navbar-title">
+        NousScan Explorer
+      </Link>
     </nav>
   );
 }
